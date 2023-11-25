85e4cf1768704bbc647f2fe06d7fffd8db9733b6
Mr. Burton <benjaminlburton@gmail.com>
2023-11-24 19:14:10 -0500

    add link

--- jsdocs/src/pages/index.js
+++ jsdocs/src/pages/index.js
@@ -4,6 +4,14 @@ import Layout from "@theme/Layout";
 export default function Home() {
   return (
     <Layout title="Home" description="Home Page">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous"
+        ></link>
+      </head>
       <div class="hero-background-wrapper">
         <section class="hero-container container">
           <header class="hero-content-container">
