d26155ecb5b55808f03341f63b8db56c56c5e558
Mr. Burton <benjaminlburton@gmail.com>
2023-11-28 00:35:12 -0500

    add algolia docusearch customization

--- jsdocs/docusaurus.config.js
+++ jsdocs/docusaurus.config.js
@@ -4,7 +4,7 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -75,7 +75,7 @@ const config = {
         },
 
         // Optional: Algolia search parameters
-        searchParameters: {},
+        searchParameters: { facetFilters: ["language:en", "version:1.0.0"] },
 
         // Optional: path for search page that enabled by default (`false` to disable it)
         searchPagePath: "search",
